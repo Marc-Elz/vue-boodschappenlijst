b26b8d6d6c7a2521a9d28577a87130f7659bc079
Marc-Elz <marcelzinga@live.nl>
2026-04-13 21:47:10 +0200

    Fix: Forget the extended map strat. Keep it simple. Using just a function for things that require properties

--- src/App.vue
+++ src/App.vue
@@ -8,34 +8,12 @@ const products = ref([
     {name: 'Noten', price: 2.99, quantity: 1},
 ]);
 
-const productSubTotals = computed(() => {
-    const subTotalArray = [];
-    for (const key in products.value) {
-        subTotalArray.push(computeSubTotal(products.value[key]));
-    }
-    console.log(subTotalArray);
-    return subTotalArray;
-
-    const subTotals = new Map();
-    for (const key in products.value) {
-        subTotals.set(key, computeSubTotal(products.value[key]));
-    }
-    console.log('subtotal: ' + subTotals.get(0));
-
-    console.log(Array.from(subTotals.values()));
-    console.log(subTotals.size);
-
-    return subTotals;
-});
-
 const productTotal = computed(() => {
-    return Array.from(productSubTotals)
-        .reduce((acc, subtotal) => acc + subtotal, 0)
-        .toFixed(2);
+    return products.value.reduce((acc, product) => acc + calcSubTotal(product), 0).toFixed(2);
 });
 
-const computeSubTotal = product => {
-    return (+product.price * +product.quantity).toFixed(2);
+const calcSubTotal = product => {
+    return +product.price * +product.quantity;
 };
 </script>
 
@@ -50,13 +28,11 @@ const computeSubTotal = product => {
         <tr v-for="(product, index) in products" :key="index">
             <td>{{ product.name }}</td>
             <td>{{ product.price }}</td>
-            <td><input type="number" v-model="product.quantity" min="0" value="0" /></td>
+            <td><input type="number" v-model.number="product.quantity" min="0" /></td>
+            <td>{{ calcSubTotal(product).toFixed(2) }}</td>
 
             <!--- Have to add the subtotals in the same row-->
         </tr>
-        <td v-for="(subTotal, index) in productSubTotals" :key="index">
-            {{ subTotal }}
-        </td>
         <tr>
             <td colspan="3">Total</td>
             <td>{{ productTotal }}</td>
