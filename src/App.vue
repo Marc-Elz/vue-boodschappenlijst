d9ae7b7c88c0731db048ffc85a6a5cf8f7b6ee65
Marc-E <marcelzinga@live.nl>
2026-04-14 10:17:42 +0200

    Moved products to parent class

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <script setup>
 import GroceriesTable from './components/GroceriesTable.vue';
+import {ref} from 'vue';
+const products = ref([
+    {name: 'Rijst', price: 1.0, quantity: 1},
+    {name: 'Brococoli', price: 0.99, quantity: 1},
+    {name: 'Koekjes', price: 1.2, quantity: 1},
+    {name: 'Noten', price: 2.99, quantity: 1},
+]);
 </script>
 
 <template>
-    <GroceriesTable />
+    <GroceriesTable v-model="products" />
 </template>
--- src/components/GroceriesTable.vue
+++ src/components/GroceriesTable.vue
@@ -28,14 +28,8 @@
 </template>
 
 <script setup>
-import {ref, computed} from 'vue';
-
-const products = ref([
-    {name: 'Rijst', price: 1.0, quantity: 1},
-    {name: 'Brococoli', price: 0.99, quantity: 1},
-    {name: 'Koekjes', price: 1.2, quantity: 1},
-    {name: 'Noten', price: 2.99, quantity: 1},
-]);
+import {computed} from 'vue';
+const products = defineModel();
 
 const productTotal = computed(() => {
     return products.value.reduce((acc, product) => acc + calcSubTotal(product), 0).toFixed(2);
