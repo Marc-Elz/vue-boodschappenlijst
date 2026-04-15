1c0f390b2a83828e334b9c0c01cc8fcdad2bc833
Marc-Elz <marcelzinga@live.nl>
2026-04-15 11:59:09 +0200

    Now also use the router after completion

--- src/domains/groceries/pages/Create.vue
+++ src/domains/groceries/pages/Create.vue
@@ -7,11 +7,15 @@
 import {addGrocery} from '../store';
 
 import GroceryForm from '../components/GroceryForm.vue';
+import {useRouter} from 'vue-router';
+
+const router = useRouter();
 
 const updateGrocery = newGrocery => {
     console.log('Handling adding of new grocery:');
     console.log(newGrocery);
     addGrocery(newGrocery);
+    router.push('/groceries/overview');
 };
 
 const grocery = {id: 7, name: '', price: 0, quantity: 0};
